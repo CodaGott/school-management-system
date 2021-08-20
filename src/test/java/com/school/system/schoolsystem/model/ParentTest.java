1517ec36a4791de6d24ee2654c0cf6584a63e7f4
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-08-20 12:31:09 +0100

    data base testing

--- src/test/java/com/school/system/schoolsystem/model/ParentTest.java
+++ src/test/java/com/school/system/schoolsystem/model/ParentTest.java
@@ -1,10 +1,49 @@
 package com.school.system.schoolsystem.model;
 
+import com.school.system.schoolsystem.repository.ParentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ParentTest {
+
+    @Autowired
+    private ParentRepository parentRepository;
+
+    @Test
+    void parentCanBeCreated(){
+        Parent parent = new Parent();
+
+        parent.setFirstName("John");
+        parent.setLastName("Doe");
+        parentRepository.save(parent);
+
+        assertEquals(parent.getFirstName(), "John");
+    }
+
+
+    @Test
+    void parentCanBeFetched(){
+        Parent parent = parentRepository.findById(5L).orElseThrow();
+        assertEquals(parent.getFirstName(), "John");
+    }
+
+    @Test
+    void parentInfoCanBeUpdated(){
+        Parent parent = parentRepository.findById(5L).orElseThrow();
+        parent.setEmail("jonny@email.com");
+        parentRepository.save(parent);
+        assertEquals(parent.getEmail(), "jonny@email.com");
+    }
+
+    @Test
+    void parentCanBeDeleted(){
+        Parent parent = parentRepository.findById(5L).orElseThrow();
+        parentRepository.delete(parent);
+        assertEquals(parentRepository.findAll().size(), 0);
+    }
 
 }

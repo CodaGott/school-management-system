b31527d57f6c5758fb2e7362b677b3177eb86084
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-08-21 20:51:53 +0100

    created teacher services

--- src/main/java/com/school/system/schoolsystem/service/TeacherService.java
+++ src/main/java/com/school/system/schoolsystem/service/TeacherService.java
@@ -1,4 +1,16 @@
 package com.school.system.schoolsystem.service;
 
+import com.school.system.schoolsystem.dto.TeacherDto;
+import com.school.system.schoolsystem.model.Teacher;
+
+import java.util.List;
+
 public interface TeacherService {
+    Teacher createTeacher(TeacherDto teacherDto);
+    Teacher getATeacher(Long teacherId);
+    List<Teacher> getTeachers();
+    void deleteATeacher(Long teacherId);
+    Teacher findATeacherByName(String teacherFirstName);
+    Teacher updateTeacherInfo(Long teacherId, TeacherDto teacherDto);
+
 }

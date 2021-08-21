bfe4fc098757af50b08c8c6f1e29952f16172d09
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-08-21 20:36:12 +0100

    course database testing

--- src/main/java/com/school/system/schoolsystem/repository/CourseRepository.java
+++ src/main/java/com/school/system/schoolsystem/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.school.system.schoolsystem.repository;
 
 import com.school.system.schoolsystem.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
--- src/test/java/com/school/system/schoolsystem/model/CourseTest.java
+++ src/test/java/com/school/system/schoolsystem/model/CourseTest.java
@@ -15,6 +15,35 @@ class CourseTest {
 
     @Test
     void courseCanBeCreated(){
+        Course course = new Course();
 
+        course.setName("English");
+        course.setScore(90);
+        course.setRemark("A student");
+        courseRepository.save(course);
+
+        assertEquals(course.getName(), "English");
     }
+
+    @Test
+    void testCourseCanBeFetched(){
+        Course course = courseRepository.findById(6L).orElseThrow();
+        assertEquals(course.getName(), "English");
+    }
+
+    @Test
+    void testCourseInfoCanBeUpdated(){
+        Course course = courseRepository.findById(6L).orElseThrow();
+        course.setName("Java");
+        courseRepository.save(course);
+        assertEquals(course.getName(), "Java");
+    }
+
+    @Test
+    void testCourseCanBeDeleted(){
+        Course course = courseRepository.findById(6L).orElseThrow();
+        courseRepository.delete(course);
+        assertEquals(courseRepository.findAll().size(), 0);
+    }
+
 }

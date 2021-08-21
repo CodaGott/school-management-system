ac8287c0e1558e427cb985e4798c4d73810ab43c
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-08-21 21:02:03 +0100

    created parent services

--- src/main/java/com/school/system/schoolsystem/service/ParentService.java
+++ src/main/java/com/school/system/schoolsystem/service/ParentService.java
@@ -1,4 +1,12 @@
 package com.school.system.schoolsystem.service;
 
+import com.school.system.schoolsystem.dto.ParentDto;
+import com.school.system.schoolsystem.model.Parent;
+
 public interface ParentService {
+    Parent createParent(ParentDto parentDto);
+    Parent updateParentInfo(Long parentId, ParentDto parentDto);
+    Parent getAParent(Long parentId);
+    Parent getParentByName(String parentName);
+    void deleteParent(Long parentId);
 }
--- src/main/java/com/school/system/schoolsystem/service/StudentService.java
+++ src/main/java/com/school/system/schoolsystem/service/StudentService.java
@@ -1,4 +1,12 @@
 package com.school.system.schoolsystem.service;
 
+import com.school.system.schoolsystem.dto.StudentDto;
+import com.school.system.schoolsystem.model.Student;
+
 public interface StudentService {
+    Student createStudent(StudentDto studentDto);
+    Student getAStudent(Long studentId);
+    Student getStudentByName(String studentFirstName);
+    Student updateStudentInfo(Long studentId, StudentDto studentDto);
+    void deleteStudentById(Long studentId);
 }

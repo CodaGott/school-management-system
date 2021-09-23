6063a07b9465dbdb23a17dfeaf9239aa9ff1d462
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-09-23 23:48:40 +0100

    made changes

--- src/main/java/com/school/system/schoolsystem/service/loginservice/LoginServices.java
+++ src/main/java/com/school/system/schoolsystem/service/loginservice/LoginServices.java
@@ -4,9 +4,14 @@ package com.school.system.schoolsystem.service.loginservice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Component
 public class LoginServices {
+
+
+
 }
